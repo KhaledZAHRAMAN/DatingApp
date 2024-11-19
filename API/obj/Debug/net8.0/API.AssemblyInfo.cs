13c908348c49caf839ed6c65bfad7a257f05a7fa
KhaledZAHRAMAN <khulio012@gmail.com>
2024-11-19 13:56:57 +0200

    add client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f627f5a6175a7f52166218874ebaa071606d01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6fad8dda1e044fcf7ac346367453afcf61a02a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
